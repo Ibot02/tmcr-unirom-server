71993d052dec97363e0649d930f3f29896fe6342
Ibot02 <jtp.jtp@gmx.de>
2025-03-08 15:43:44 +0100

    React to save and re-give unsaved items on login]

--- app/Items.hs
+++ app/Items.hs
@@ -17,6 +17,7 @@ import Control.Concurrent.STM
 import qualified StmContainers.Map as TM
 
 import Control.Applicative
+import Control.Monad
 
 import qualified Data.ByteString.Lazy as BS
 
@@ -46,7 +47,7 @@ type Placements = Map WorldID (Map Flag Placement)
 itemsHandler :: FilePath -> Handler (TBQueue Packet, TQueue Packet) ()
 itemsHandler f = withResources (acqG f) relG acqR relR $ h
 
-type G = (Placements, TM.Map Word8 (TQueue Item))
+type G = (Placements, TM.Map Word8 (TQueue Item, TQueue Item))
 type R = (TMVar Word8, TMVar Item)
 
 acqG :: FilePath -> IO G
@@ -66,15 +67,29 @@ relR (_, queues) (whoAmI, lastSent) = atomically $ (isEmptyTMVar lastSent >>= ch
   unGetTQueue q i
 
 h :: Handler (G, R, (TBQueue Packet, TQueue Packet)) ()
-h = liftSTM $ \i -> handleOutbound i <|> handleLogin i <|> handlePeek i <|> handleLookup i
+h = liftSTM $ \i -> handleOutbound i <|> handleLoginOrSave i <|> handlePeek i <|> handleLookup i
 
-handleLogin :: (G, R, (TBQueue Packet, TQueue Packet)) -> STM ()
-handleLogin ((_, _),(whoAmI, _),(inQ, outQ)) = do
+handleLoginOrSave :: (G, R, (TBQueue Packet, TQueue Packet)) -> STM ()
+handleLoginOrSave ((_, qs),(whoAmI, _),(inQ, outQ)) = do
   (_, _, d) <- handlePacket SendItem inQ outQ
-  case d of
-    [w] -> do
-      writeTMVar whoAmI w
-    _ -> return ()
+  handleLogin whoAmI d qs <|> handleSave whoAmI qs
+
+handleLogin :: TMVar Word8 -> [Word8] -> TM.Map Word8 (TQueue Item, TQueue Item) -> STM ()
+handleLogin whoAmI [w] qs = do
+  writeTMVar whoAmI w
+  q' <- TM.lookup w qs
+  case q' of
+    Nothing -> return ()
+    Just (q, floating) -> do
+      items <- flushTQueue floating
+      forM_ items $ \item -> writeTQueue q item >> writeTQueue floating item
+handleLogin _ _ _ = error "unexpected login message"
+
+handleSave :: TMVar Word8 -> TM.Map Word8 (TQueue Item, TQueue Item) -> STM ()
+handleSave whoAmI qs = do
+  (_, floating) <- readTMVar whoAmI >>= getQueues qs
+  _ <- flushTQueue floating
+  return ()
 
 handlePeek :: (G, R, (TBQueue Packet, TQueue Packet)) -> STM ()
 handlePeek ((placements, _), (whoAmI, _), (inQ, outQ)) = ignoreAck Peek inQ <|> do
@@ -114,20 +129,24 @@ encodeLE16 w = (fromIntegral (w .&. 0xFF), fromIntegral (w `shiftR` 8))
 decodeLE16 :: (Word8, Word8) -> Word16
 decodeLE16 (b, b') = fromIntegral b .|. (fromIntegral b' `shiftL` 8)
 
-getQueue ::  TM.Map WorldID (TQueue Item) -> WorldID -> STM (TQueue Item)
-getQueue qs w = do
+getQueue ::  TM.Map WorldID (TQueue Item, TQueue Item) -> WorldID -> STM (TQueue Item)
+getQueue qs w = fmap fst $ getQueues qs w
+getQueues ::  TM.Map WorldID (TQueue Item, TQueue Item) -> WorldID -> STM (TQueue Item, TQueue Item)
+getQueues qs w = do
   c <- TM.lookup w qs
   case c of
     Nothing -> do
       c' <- newTQueue
-      TM.insert c' w qs
-      return c'
+      c'' <- newTQueue
+      TM.insert (c', c'') w qs
+      return (c', c'')
     Just c' -> return c'
 
-queueItem :: TM.Map WorldID (TQueue Item) -> WorldID -> Item -> STM ()
+queueItem :: TM.Map WorldID (TQueue Item, TQueue Item) -> WorldID -> Item -> STM ()
 queueItem qs w i = do
-  q <- getQueue qs w
+  (q, q') <- getQueues qs w
   writeTQueue q i
+  writeTQueue q' i
 
 ignoreAck :: Port -> TBQueue Packet -> STM ()
 ignoreAck = handleAck (const (return ()))
@@ -139,7 +158,7 @@ handleOutbound ((_, items),(whoAmI, lastSent),(inQ, outQ)) = sendOutbound <|> ha
     c <- TM.lookup w items
     i <- case c of
       Nothing -> retry
-      Just c' -> readTQueue c'
+      Just (c', _) -> readTQueue c'
     putTMVar lastSent i
     sendItem' i
   handleAck' = handleAck (const $ takeTMVar lastSent >> return ()) SendItem inQ
